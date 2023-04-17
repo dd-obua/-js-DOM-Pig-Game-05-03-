83f7e71ad5e516b0704ab8611ed1c71ed8a48370
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-17 20:59:40 +0300

    Remove the console log

--- js/script.js
+++ js/script.js
@@ -6,15 +6,19 @@ const score0Element = select('#score--0');
 const score1Element = select('#score--1');
 const diceElement = select('.dice');
 const btnRollDice = select('.roll-dice');
+const currentScore1Text = select('.current-score-1');
+const currentScore2Text = select('.current-score-2');
 
 score0Element.textContent = 0;
 score1Element.textContent = 0;
 
 const rollDice = () => Math.trunc(Math.random() * 6) + 1;
 
+let currentScore1 = 0;
+let currentScore2 = 0;
+
 btnRollDice.addEventListener('click', function () {
   const randomDiceRoll = rollDice();
-  console.log(randomDiceRoll, typeof randomDiceRoll);
 
   // Display rolled dice
   diceElement.classList.remove('hidden');
