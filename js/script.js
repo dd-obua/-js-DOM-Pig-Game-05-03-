c13f0064d2fac467182c830a10d38b32d2008289
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-17 21:14:59 +0300

    Update current score on dice roll for Player 1

--- js/script.js
+++ js/script.js
@@ -6,16 +6,14 @@ const score0Element = select('#score--0');
 const score1Element = select('#score--1');
 const diceElement = select('.dice');
 const btnRollDice = select('.roll-dice');
-const currentScore1Text = select('.current-score-1');
-const currentScore2Text = select('.current-score-2');
-
-score0Element.textContent = 0;
-score1Element.textContent = 0;
+const current0Element = select('#current--0');
+const current1Element = select('#current--1');
 
 const rollDice = () => Math.trunc(Math.random() * 6) + 1;
 
-let currentScore1 = 0;
-let currentScore2 = 0;
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+let currentScore = 0;
 
 btnRollDice.addEventListener('click', function () {
   const randomDiceRoll = rollDice();
@@ -26,12 +24,13 @@ btnRollDice.addEventListener('click', function () {
 
   if (randomDiceRoll !== 1) {
     // Update current score 1
-    currentScore1 += randomDiceRoll;
-
+    currentScore += randomDiceRoll;
     // Display current score 1
-    currentScore1Text.textContent = currentScore1;
+    current0Element.textContent = currentScore;
   } else {
-    currentScore1 = 0;
-    currentScore1Text.textContent = currentScore1;
+    currentScore += randomDiceRoll;
+
+    currentScore = 0;
+    current0Element.textContent = currentScore;
   }
 });
