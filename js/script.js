bf198cedee2983eb85b730de4365c945b953ec25
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-17 20:05:36 +0300

    Select the different dice roll outcomes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 const select = selector => document.querySelector(selector);
+const dice = select('.dice');
+const dice1 = select('.dice-1');
+const dice2 = select('.dice-2');
+const dice3 = select('.dice-3');
+const dice4 = select('.dice-4');
+const dice5 = select('.dice-5');
+const dice6 = select('.dice-6');
 
 const btnRollDice = select('.roll-dice');
 
