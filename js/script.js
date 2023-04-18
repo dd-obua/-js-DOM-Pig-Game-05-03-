bacdb25abe67330bfbc85b92782fd43ac9c6219a
Obua Denis Daniel <dd.obua@gmail.com>
2023-04-18 10:33:03 +0300

    Reorganize code into different functionality blocks

--- js/script.js
+++ js/script.js
@@ -1,36 +1,30 @@
 'use-strict';
 
+// Select elements
 const select = selector => document.querySelector(selector);
 
-const score0Element = select('#score--0');
-const score1Element = select('#score--1');
-const diceElement = select('.dice');
 const btnRollDice = select('.roll-dice');
+const diceElement = select('.dice');
 const current0Element = select('#current--0');
 const current1Element = select('#current--1');
+const score0Element = select('#score--0');
+const score1Element = select('#score--1');
 
-const rollDice = () => Math.trunc(Math.random() * 6) + 1;
-
+// Starting conditions
 score0Element.textContent = 0;
 score1Element.textContent = 0;
 let currentScore = 0;
 
+const rollDice = () => Math.trunc(Math.random() * 6) + 1;
+
 btnRollDice.addEventListener('click', function () {
-  const randomDiceRoll = rollDice();
+  const rolledDice = rollDice();
 
-  // Display rolled dice
   diceElement.classList.remove('hidden');
-  diceElement.src = `./img/dice-${randomDiceRoll}.png`;
+  diceElement.src = `./img/dice-${rolledDice}.png`;
 
-  if (randomDiceRoll !== 1) {
-    // Update current score 1
-    currentScore += randomDiceRoll;
-    // Display current score 1
-    current0Element.textContent = currentScore;
-  } else {
-    currentScore += randomDiceRoll;
-
-    currentScore = 0;
+  if (rolledDice !== 1) {
+    currentScore += rolledDice;
     current0Element.textContent = currentScore;
   }
 });
